1b9802031e2326afb1ca3a5fa06c1b76771cbd5b
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-04-14 21:12:06 +0530

    Updates flakes with Docker Image Creation.

--- flake.nix
+++ flake.nix
@@ -1,36 +1,64 @@
 {
-  description = "Syswatch - Rust System Monetoring Tool For servers, with Nix Deployment.";
+  description = "SysWatch - Quick System Monetoring tool for servers build with Rust + Nix Deployment ";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    fenix.url = "github:nix-community/fenix";
   };
 
-  outputs = { self, nixpkgs }: {
-    let system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-    in{
+  outputs = { self, nixpkgs, fenix }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ fenix.overlays.default ];
+      };
+      rustToolchain = pkgs.fenix.complete.toolchain;
+    in
+    {
+      # Devshell 
+      devShells.${system}.default =
+        pkgs.mkShell {
+          buildInputs = with pkgs; [ rustToolchain ];
+          shellHook = '' echo "🚀 SysWatch DevShell (FenixRust)" '';
+        };
 
-    # DevShell
-    devShell.${system}.default = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        rustc
-        cargo
-        rustfmt
-        clippy
-      ];
-    };
+      packages.${system} = {
 
-    # Build pkgs
-    packages.${system}.default = pkgs.rustPlatform.buildRustPackages {
-      pname = "syswatch";
-      version = "0.1.0";
-      src = ./.;
-      cargoLock = {
-        locaFile = ./Cargo.lock
+        # build Pkgs
+        default =
+          pkgs.rustPlatform.buildRustPackage {
+            pname = "SysWatch";
+            version = "0.1.0";
+            src = ./.;
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+
           };
 
-      };
-    };
+        # DockerTools
+        dockerImage =
+          pkgs.dockerTools.buildImage
+            {
+              name = "SysWatch";
+              tag = "latest";
 
-  };
+              copyToRoot = pkgs.buildEnv {
+                name = "image-root";
+                paths = [ self.packages.${system}.default ];
+                pathsToLink = [ "/bin" ];
+              };
+              # contents = [ self.packages.${system}.default ];
+              # config = { Cmd = [ "${self.packages.${system}.default}/bin/syswatch" ]; };
+
+              config =
+                {
+                  Cmd = [ "syswatch" ];
+                };
+
+            };
+      };
+
+    };
 }
