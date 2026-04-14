31e2b8d673f1f8cd521fe22db14b8be7818e4b20
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-04-14 20:14:30 +0530

    update services

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-
 mod config;
 mod errors;
 mod handlers;
+
+// struct SysStat
 mod models;
+
+// collector_main
 mod services;
+use services::system_collector::collect_system_stats;
+
 mod utils;
 
 /// Entry Point
@@ -14,11 +19,14 @@ mod utils;
 /// > commands
 /// ```bash
 ///
-///
-// ```
+/// ```
 fn main() {
-    services::system_collector::main();
+    let stats = collect_system_stats();
+    let divider: u64 = 1024 * 1024;
+
+    println!("------------SystemStats------------");
+    println!("CPU usage: {}%", stats.cpu_usage);
+    println!("Memory Usage: {} /{} mb", stats.memory_usage / &divider, stats.memory_total / &divider);
+    println!("Processes: {}", stats.process_count);
+    println!("Uptime: {}sec", stats.uptime);
 }
-
-
-
